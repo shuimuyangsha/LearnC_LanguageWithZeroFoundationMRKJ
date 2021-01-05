2953141db228b23aa4f89a692184ac48f7be53a6
shuimuyangsha <1023178387@qq.com>
2021-01-05 21:39:45 +0800

    ~构建第15章 文件

--- Code/SL/15/01/1.C
+++ Code/SL/15/01/1.C
@@ -4,7 +4,7 @@ void main()
 {
     FILE *fp;										/*定义一个指向FILE类型结构体的指针变量*/
     char ch;										/*定义变量为字符型*/
-    if ((fp = fopen("E:\\exp01.txt", "w")) == NULL)			/*以只写方式打开指定文件*/
+    if ((fp = fopen("E:\\我的下载\\exp01.txt", "w")) == NULL)			/*以只写方式打开指定文件*/
     {
         printf("cannot open file\n");
         exit(0);
